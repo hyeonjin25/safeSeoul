6d7fbcfce85e5cc4b7ba7127eb64d77476092c35
sohyeonjin <shj41738866@gmail.com>
2021-05-11 00:56:58 +0900

    Add router and link Header and Footer

---
+++ client/src/component/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div style={{ backgroundColor: "#BCE55C" }}>
+      <div
+        style={{
+          padding: "20px",
+          textAlign: "center",
+          fontSize: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        SafeSeoul
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ client/src/router/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/reset.css";
+
+function Main() {
+  return <div>메인</div>;
+}
+
+export default Main;
