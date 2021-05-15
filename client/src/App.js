0d133a1f99099d31b085feb7cc8a0b9d77ec8f27
sohyeonjin <shj41738866@gmail.com>
2021-05-15 14:33:02 +0900

    Remove header and footer in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Main from "./router/Main";
+import Map from "./router/Map";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div style={{ minHeight: "70vh", padding: "20px 0 20px 0" }}>
+      {/* <Header /> */}
+      <div>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/map" component={Map} />
         </Switch>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
